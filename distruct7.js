3a764b7eda8dc0ab42197cd5cc96bb836d1550be
unknown <shahriarnaiemrafi94@gmail.com>
2026-07-28 23:09:51 +0400

    advance object and array distructuring

---
+++ distruct7.js
@@ -0,0 +1,36 @@
+
+// const product ={name: "shirt", price: 25.00, inStock: true};
+
+// // const discount=product.price*20/100;
+// // const yourpay=product.price-discount;
+// // const vatamount=product.price*7/100;
+// // const totalamount=yourpay+ vatamount;
+// // console.log(`Your pay is ${yourpay} and total amount is ${totalamount}`);
+
+// // const price=product.price;
+
+// // const yourpay=price*20/100;
+// // const vatamount=price*7/100;
+// // const totalamount=yourpay+ vatamount;
+
+
+// const price=product.price;
+
+// const yourpay=price*20/100;
+// const vatamount=price*7/100;
+// const totalamount=yourpay+ vatamount;
+/**
+const device={name: "phone", price: 2500.00, quantity: 2};
+// kono object ar property ke variable ar moto use korte hole destructuring korte hoy.
+// atai destructuring bole.
+const {quantity}=device;
+console.log(quantity);
+
+
+ */  
+const numbers=[6, 9,20,44];
+const [first,second]=numbers;
+console.log(first,second);
+
+const [math,science]=[20,90];
+console.log(science);
